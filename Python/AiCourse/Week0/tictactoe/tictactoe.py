eb3bc83f5e89fe8ef2bac4f476739fa0ddebacd1
ZeroHoots <mros7116@uni.syndey.edu.au>
2020-05-16 18:50:03 +1000

    z

--- Python/AiCourse/Week0/tictactoe/tictactoe.py
+++ Python/AiCourse/Week0/tictactoe/tictactoe.py
@@ -99,7 +99,7 @@ def winnerEval(board):
         if (board[0][i] == board[1][i] == board [2][i]) and board[0][i] is not None:
             win = board[0][i]
         
-        #diagssssss
+        #diags
         if( board[0][0] == board[1][1] == board[2][2]) and board[0][0] is not None:
             win =  board[0][0]
         if (board[0][2] == board[1][1] == board [2][0]) and board [1][1] is not None:
