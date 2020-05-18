04438cfd3d3bca983e4e89548312abfb3cfc6f81
ZeroHoots <mros7116@uni.syndey.edu.au>
2020-05-18 17:47:17 +1000

    Knights

--- Python/AiCourse/Week1/knights/puzzle.py
+++ Python/AiCourse/Week1/knights/puzzle.py
@@ -12,29 +12,61 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnave, AKnight),
+    Implication(AKnave, Not(And(AKnave, AKnight))),
+    Implication(AKnight, And(AKnave, AKnight)),
 )
+
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Or(AKnave, AKnight),
+    Or(BKnave, BKnight),
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
+    Or(AKnave, AKnight),
+    Or(BKnave, BKnight),
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnave, Or(And(AKnight, BKnight), And(AKnave, BKnave)))
     # TODO
 )
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
-# B says "C is a knave."
-# C says "A is a knight."
+# B says "C is a knave." c
+# C says "A is a knight." c
 knowledge3 = And(
+    Or(AKnave, AKnight),
+    Or(BKnave, BKnight),
+    Or(CKnave, CKnight),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    Implication(CKnight, AKnight),
+    Implication(CKnave, AKnave),
+    Implication(BKnight, AKnave),
+    Implication(BKnave, Not(AKnave)),
+    Implication(
+        BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, AKnight))
+    ),
+    Implication(
+        BKnave, Not(Or(Implication(AKnight, AKnave), Implication(AKnave, AKnight)))
+    ),
+    Or(
+        And(Implication(AKnight, AKnight), Implication(AKnave, AKnave)),
+        And(Implication(AKnave, AKnight), Implication(AKnight, AKnave)),
+    )
     # TODO
 )
 
@@ -45,7 +77,7 @@ def main():
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
         ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle 3", knowledge3),
     ]
     for puzzle, knowledge in puzzles:
         print(puzzle)
