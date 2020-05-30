12b27b52d47ab265914c4620ef0c42634f501b7b
ZeroHoots <mros7116@uni.syndey.edu.au>
2020-05-30 14:16:28 +1000

    What r u doing

---
+++ Python/fun_projects/Word_finder_AND_VENV_EXAMPLES/recursive_wordfinder.py
@@ -0,0 +1,31 @@
+#conda create -n shiny_new_env
+#conda env list
+#conda activate shiny_new_env
+#pip freeze > requirements.txt
+
+#pip install -r requirements.txt
+#https://medium.com/@boscacci/why-and-how-to-make-a-requirements-txt-f329c685181e
+
+
+
+
+
+
+#ANd now, onto the program
+import nltk
+#nltk.download('words')
+from nltk.corpus import words
+#"fine" in words.words()
+
+
+def drop_char_index(s, i):
+    if i == 0:
+        return s[1:]
+    elif i == len(s) - 1:
+        return s[:i]
+    else:
+        return s[:i] + s[i+1:]
+
+
+def recurse(word):
+    
